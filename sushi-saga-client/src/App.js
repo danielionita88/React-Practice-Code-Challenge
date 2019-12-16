ddb3b3dc55d7568083eefdb64ce9996ab0bb3c41
Daniel Ionita <danielionita88@yahoo.com>
2019-12-16 13:46:33 -0600

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,52 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state={
+      page: 0,
+      sushis:[],
+      amount: 100,
+      eaten: []
+    }
+  }
+
+  handleMoreBtn=()=>{
+    this.setState({
+      page: this.state.page + 1
+    })
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data =>{
+      this.setState({
+        sushis: data
+      })}
+    )
+  }
+
+  handleEaten = (sushi)=>{
+    const newMoney = this.state.amount - sushi.price
+    if(newMoney >= 0 && !this.state.eaten.includes(sushi)){
+      this.setState({
+        amount: newMoney,
+        eaten: [...this.state.eaten, sushi]
+      })
+    }
+  }
+
+
   render() {
+    const sushis=this.state.sushis
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={sushis.slice(4*this.state.page, (4*this.state.page)+4)} 
+        handleMoreBtn={this.handleMoreBtn}
+        handleEaten={this.handleEaten}
+        eaten={this.state.eaten}/>
+        <Table eaten={this.state.eaten} amount={this.state.amount} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushis.map((sushi,index)=><Sushi key={index} eaten={props.eaten} sushi={sushi} handleEaten={props.handleEaten}/>)}
+        <MoreButton handleMoreBtn={props.handleMoreBtn}/>
       </div>
     </Fragment>
   )
